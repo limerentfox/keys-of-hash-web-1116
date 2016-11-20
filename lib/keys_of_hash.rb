d416d3cd1e3d2b0882d90b7963a6c1fa03971cf4
limerentfox <christi@limerentfox.com>
2016-11-20 07:04:26 -0500

    keys-of-hash lab complete

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,14 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    array = []
+    self.map do |k,v|
+      arguments.each do |arg|
+        if v == arg
+        array << k
+        end
+      end
+  end
+  array
   end
 end
